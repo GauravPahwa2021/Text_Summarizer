e2032ef3ba59c7c7280627303fcd66e97a51ae9a
Gaurav Pahwa <2020uce1603@mnit.ac.in>
2023-06-03 14:14:07 +0530

    utils

--- src/Text_Summarizer/utils/common.py
+++ src/Text_Summarizer/utils/common.py
@@ -5,7 +5,6 @@ from Text_Summarizer.logging import logger
 from Text_Summarizer.exception_handler import CustomException
 from ensure import ensure_annotations
 from box import ConfigBox
-
 from pathlib import Path
 from typing import Any
 
@@ -18,4 +17,18 @@ def read_yaml(path_to_yaml:Path) -> ConfigBox:
             return ConfigBox(content)
     except Exception as e:
         raise CustomException(e,sys)
-
+    
+@ensure_annotations
+def create_directories(path_to_directories: list, verbose=True):
+    try:
+        for path in path_to_directories:
+            os.makedirs(path, exist_ok=True)
+            if verbose:
+                logger.info(f"created directory at: {path}")
+    except Exception as e:
+        raise CustomException(e,sys)
+    
+@ensure_annotations
+def get_size(path: Path) -> str:
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
